86102aaa8e69b975f1b11d3b023388835c229acd
Sebastian Gomez <sebgomez395@gmail.com>
2025-12-02 18:42:01 -0500

    actualizacion del pages

--- src/app/pokemon/pages/pokemon-detail-page/pokemon-detail-page.ts
+++ src/app/pokemon/pages/pokemon-detail-page/pokemon-detail-page.ts
@@ -1,29 +1,34 @@
-import { Component, inject, input, resource } from '@angular/core';
-import { PokemonService } from '../../services/pokemon';
+import { CommonModule } from '@angular/common';
+import { Component, inject, resource } from '@angular/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { PokemonService } from '../../services/pokemon';
 
 @Component({
   selector: 'app-pokemon-detail-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './pokemon-detail-page.html',
   styleUrls: ['./pokemon-detail-page.css'],
 })
 export class PokemonDetailPageComponent {
   private pokemonService = inject(PokemonService);
-  
-  id = input.required<string>();
+  private route = inject(ActivatedRoute);
 
   pokemonResource = resource({
-    params: () => ({ id: this.id() }),
-    
+    params: () => ({ id: this.route.snapshot.paramMap.get('id') ?? '' }),
+
     loader: async ({ params }) => {
-      return firstValueFrom(
+      if (!params.id) {
+        throw new Error('ID de Pokémon no proporcionado');
+      }
+      return await firstValueFrom(
         this.pokemonService.getPokemonDetail(params.id)
       );
     }
   });
 
-  hasHiddenAbility(abilities: any[]): boolean {
+  hasHiddenAbility(abilities: any[] = []): boolean {
     return abilities.some(a => a.is_hidden);
   }
 }
